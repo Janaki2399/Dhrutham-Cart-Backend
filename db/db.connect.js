45dbac0d99005628427553ca1b0dab9044f81fd4
janaki2399 <janakishreeja23@gmail.com>
2021-04-28 18:47:40 +0530

    refactor

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-const CONNECTION_URL=process.env.REACT_APP_CONNECTION_URL;
+// const CONNECTION_URL=process.env.REACT_APP_CONNECTION_URL;
  function mongoDBConnection() {
   
-  mongoose.connect("mongodb+srv://janaki2399:tinku456@janaki.k7yop.mongodb.net/inventory?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+  mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=>console.log("connected"))
   .catch((err)=>console.log(err))
   
