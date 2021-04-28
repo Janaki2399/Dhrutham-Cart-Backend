d0c72a757332287ec9064d2073dc4268a54adef4
janaki2399 <janakishreeja23@gmail.com>
2021-04-28 18:27:05 +0530

    refactor

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
 const CONNECTION_URL=process.env.REACT_APP_CONNECTION_URL;
-async function mongoDBConnection() {
-  try{
-    const response=await mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-  }
-  catch(err){
-    console.log(err);
-  }
+ function mongoDBConnection() {
+  
+  mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=>console.log("connected"))
+  .catch((err)=>console.log(err))
+  
 }
 
 module.exports={mongoDBConnection}
