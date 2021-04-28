733db3515df1a1b037d7e39a4638dd04e7bb5b28
janaki2399 <janakishreeja23@gmail.com>
2021-04-28 18:42:49 +0530

    refactor

--- db/db.connect.js
+++ db/db.connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const CONNECTION_URL=process.env.REACT_APP_CONNECTION_URL;
  function mongoDBConnection() {
   
-  mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  mongoose.connect("mongodb+srv://janaki2399:tinku456@janaki.k7yop.mongodb.net/inventory?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=>console.log("connected"))
   .catch((err)=>console.log(err))
   
